4c0215443d7c165a91eb92cf6e85c9af81cb465e
Jani Koponen <jkopsutin@gmail.com>
2024-01-04 15:21:35 +0200

    Add tests for RectExtension

---
+++ test/extensions/rect_extension_test.dart
@@ -0,0 +1,55 @@
+import 'package:fl_chart/src/extensions/rect_extension.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('RectExtension', () {
+    group('addPadding', () {
+      test('should not affect with EdgeInsets.zero', () {
+        const rect = Rect.fromLTWH(1, 1, 1, 1);
+        const padding = EdgeInsets.zero;
+
+        expect(rect.applyPadding(padding), rect);
+      });
+
+      // Adjust tooltip start heights?
+
+      test('should add correct amount of padding', () {
+        const rect = Rect.fromLTRB(25, 50, 100, 100);
+        const padding = EdgeInsets.symmetric(vertical: 16, horizontal: 8);
+        expect(
+          rect.applyPadding(padding),
+          Rect.fromLTRB(
+            25 - padding.left,
+            50 - padding.top,
+            100 + padding.right,
+            100 + padding.bottom,
+          ),
+        );
+      });
+    });
+
+    group('addMargin', () {
+      test('should not affect with EdgeInsets.zero', () {
+        const rect = Rect.fromLTWH(1, 1, 1, 1);
+        const margin = EdgeInsets.zero;
+
+        expect(rect.applyMargin(margin), rect);
+      });
+
+      test('should move rect the correct amount', () {
+        const rect = Rect.fromLTRB(25, 250, 100, 300);
+        const margin = EdgeInsets.symmetric(vertical: 16, horizontal: 8);
+        expect(
+          rect.applyMargin(margin),
+          Rect.fromLTRB(
+            25 + margin.horizontal,
+            250 - margin.vertical,
+            100 + margin.horizontal,
+            300 - margin.vertical,
+          ),
+        );
+      });
+    });
+  });
+}
